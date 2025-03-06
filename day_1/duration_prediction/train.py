bc0121f640d651f9d77736cceb4f48cd878ca4ce
ynotzort <ynotzort@users.noreply.github.com>
2025-03-06 14:14:24 +0000

    use argparse

--- day_1/duration_prediction/train.py
+++ day_1/duration_prediction/train.py
@@ -3,9 +3,11 @@
 
 from datetime import date
 import pandas as pd
+
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 import pickle
+import argparse
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
@@ -16,19 +18,19 @@ from sklearn.pipeline import make_pipeline
 def read_dataframe(filename):
     df = pd.read_parquet(filename)
 
-    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df["duration"] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
     df.duration = df.duration.dt.total_seconds() / 60
 
     df = df[(df.duration >= 1) & (df.duration <= 60)]
 
-    categorical = ['PULocationID', 'DOLocationID']
+    categorical = ["PULocationID", "DOLocationID"]
     df[categorical] = df[categorical].astype(str)
-    
+
     return df
 
 
 def run(train_date: date, val_date: date, out_path: str):
-    base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet'
+    base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet"
 
     train_url = base_url.format(year=train_date.year, month=train_date.month)
     val_url = base_url.format(year=val_date.year, month=val_date.month)
@@ -41,32 +43,48 @@ def run(train_date: date, val_date: date, out_path: str):
     print(f"Length of Training data: {len(df_train)}")
     print(f"Length of Validation data: {len(df_val)}")
 
-    categorical = ['PULocationID', 'DOLocationID']
-    numerical = ['trip_distance']
+    categorical = ["PULocationID", "DOLocationID"]
+    numerical = ["trip_distance"]
 
-    train_dicts = df_train[categorical + numerical].to_dict(orient='records')
-    val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+    train_dicts = df_train[categorical + numerical].to_dict(orient="records")
+    val_dicts = df_val[categorical + numerical].to_dict(orient="records")
 
-    target = 'duration'
+    target = "duration"
     y_train = df_train[target].values
     y_val = df_val[target].values
 
-
-    pipeline = make_pipeline(
-        DictVectorizer(),
-        LinearRegression()
-    )
+    pipeline = make_pipeline(DictVectorizer(), LinearRegression())
     pipeline.fit(train_dicts, y_train)
     y_pred = pipeline.predict(val_dicts)
 
     print(f"MSE: {mean_squared_error(y_val, y_pred, squared=False)}")
 
     print(f"saving model to {out_path}")
-    with open(out_path, 'wb') as f_out:
+    with open(out_path, "wb") as f_out:
         pickle.dump(pipeline, f_out)
 
-if __name__ == "__main__":
-    train_date = date(2022, 1, 1)
-    val_date = date(2022, 2, 1)
-    run(train_date, val_date, "lin_reg.bin")
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Train a model on specified dates and save it to a file."
+    )
+    parser.add_argument(
+        "--train-date", required=True, help="Training Month in the format YYYY-MM"
+    )
+    parser.add_argument(
+        "--val-date", required=True, help="Validation Month in the format YYYY-MM"
+    )
+    parser.add_argument(
+        "--model-save-path",
+        required=True,
+        help="Path where the trained model will be saved",
+    )
+
+    args = parser.parse_args()
+    train_year, train_month = args.train_date.split("-")
+    val_year, val_month = args.val_date.split("-")
+    model_save_path = args.model_save_path
+
+    train_date = date(int(train_year), int(train_month), 1)
+    val_date = date(int(val_year), int(val_month), 1)
+    run(train_date, val_date, model_save_path)
